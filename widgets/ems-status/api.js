b5ab59a304cae7f5ccd9a72803c3366553abbee8
Menno <qfsdhh98z4@privaterelay.appleid.com>
2026-05-29 07:22:59 +0200

    v1.1.3: EMS Status toont plan morgen, Dashboard layout compacter

--- widgets/ems-status/api.js
+++ widgets/ems-status/api.js
@@ -4,4 +4,9 @@ module.exports = {
   async getState({ homey }) {
     return homey.app.ems.getPublicState();
   },
+  async getPlan({ homey }) {
+    return homey.app.ems.planningEngine
+      ? homey.app.ems.planningEngine.getCurrentPlan()
+      : null;
+  },
 };
